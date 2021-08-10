feb35ccc3b930ef39985702d08898e85299a64d9
Alula <altsyset@gmail.com>
2021-08-10 05:04:06 +0300

    Generated angular frontend for the client app.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # MEAN Stack Starter
-This starter boiler plate simply implements users, roles and permissions using MEAN stack. Use it to start a project using MongoDB,
+This starter boilerplate simply implements users, roles and permissions using MEAN stack. Use it to start a project using MongoDB,
+
+- A simple CRUD on a supplies sales mongo list
+- Pagination, sorting, filtering and search.
+- 
+
+## Tech stack.
+
+### Server
+
+ - NodeJs,
+ - Express,
+ - Mongoose.
+
+ ### Client
+
+ - Angular.
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { SalesComponent } from './sales/sales.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    NavigationComponent,
+    SalesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
